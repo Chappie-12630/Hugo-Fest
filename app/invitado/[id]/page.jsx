b1f8538a4997c266cf1fa6f1d90b2a09536eaa4d
Chappie-12630 <espirigonzalesfalsh1@gmail.com>
2026-04-27 16:47:12 -0700

    Update page.jsx

--- app/invitado/[id]/page.jsx
+++ app/invitado/[id]/page.jsx
@@ -1,5 +1,4 @@
 // app/invitado/[id]/page.jsx
-"use client";
 import InvitacionPublica from "../../../components/InvitacionPublica";
 export default function InvitadoPage({ params }) {
   return <InvitacionPublica guestId={params.id} />;
